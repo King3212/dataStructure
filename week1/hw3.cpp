c97a0758d324eb1c1753fe59c64a87aaec7f8a1c
King3212 <2591579996@qq.com>
2023-09-24 13:23:29 +0000

    it is good for week1

--- week1/hw3.cpp
+++ week1/hw3.cpp
@@ -16,35 +16,40 @@ class LinkList{
     public:
         void show(){
             Node<T> *temp = tail->next;
-            while(temp){
+            while(temp != tail){
                 cout << temp->data << " ";
                 temp = temp-> next;
             }
-            cout << endl;
+            cout << temp->data << endl;
         }
         LinkList(T x);
         LinkList();
         bool pushback(T x);
         bool pushforward(T x);
         bool sortedInsert(T x);
+        bool deleteItem(T x);
+
 
 };
+
+
+
 
 template<class T>
 bool LinkList<T>::sortedInsert(T x){
     if(size == 0){
-        pushforward(x);
         size ++;
-        return true;
+        return pushforward(x);
     }
     try {
-        if(x < head->data){
-            pushforward(x);
+        if(x < tail->next->data){
             size ++;
-            return true;
+            return pushforward(x);
+        }else if(x > tail->data){
+            return pushback(x);
         }
-        Node<T> *temp = head,*keep = head;
-        while(temp && x > temp->data){
+        Node<T> *temp = tail->next,*keep = tail;
+        while(temp!= tail->next->next && x > temp->data){
             keep = temp;
             temp = temp->next;
         }
@@ -61,21 +66,20 @@ bool LinkList<T>::sortedInsert(T x){
         return false;
     }
 }
+
+
+
 template <class T>
 LinkList<T>::LinkList(T x){
-    head = new Node<T>();
-    head->next = nullptr;
     Node<T> *temp = new Node<T>();
+    tail = temp;
     temp->data = x;
-    temp->next = nullptr;
-    head->next = temp;
-    tail->next = temp;
+    temp->next = tail;
     size = 1;
 }
 
 template <class T>
 LinkList<T>::LinkList(){
-    head = nullptr;
     tail = nullptr;
     size = 0;
 }
@@ -87,12 +91,13 @@ bool LinkList<T>::pushback(T x){
     if(tail == nullptr)
     {
         tail = temp;
-        head = temp;
+        temp -> next = temp;
         size++;
         return true;
     }
-    else
+    else if(size > 0)
     {
+        temp->next = tail->next;
         tail->next = temp;
         tail = temp;
         size++;
@@ -105,11 +110,38 @@ template <class T>
 bool LinkList<T>::pushforward(T x){
     Node<T> *temp = new Node<T>();
     temp->data = x;
-    temp->next = head;
-    head = temp;
-    size++;
-    return true;
+    if(size == 0){
+        size ++;
+        return pushback(x);
+    }
+    else{
+        temp->next = tail->next;
+        tail->next = temp;
+        size ++;
+        return true;
+    }
+    return false;
+
 }
+
+
+
+template <class T>
+bool LinkList<T>::deleteItem(T x){
+    Node<T> *temp = tail->next,*keep;
+    while(temp && temp != tail->next){
+        keep = temp;
+        if(temp->data == x){
+            keep->next = temp->next;
+            size--;
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+    
 
 int main(){
     LinkList<int> one = LinkList<int>();
